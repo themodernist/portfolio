a0d395c5cf6e4f244b2876f77f456264a8524f7e
Caroline Guntert <carolineguntert@gmail.com>
2017-05-23 16:29:00 -0400

    hello

--- me/src/App.js
+++ me/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-       <div className"coming">
-          <h2>Coming Soon</h2>
+       <div id="root">
+       <div class="parallax"></div>
+          <h2>Coming Soon <span>.</span> . .</h2>
+          <div class="parallax"></div>
        </div>
    );
   }
