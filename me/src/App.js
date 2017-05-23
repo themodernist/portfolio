1f04b41f46f5da560b82c5ff2183f40da50b7d42
Caroline Guntert <carolineguntert@gmail.com>
2017-05-23 15:58:29 -0400

    dosen't matter

---
+++ me/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+       <div className"coming">
+          <h2>Coming Soon</h2>
+       </div>
+   );
+  }
+}
+
+export default App;
